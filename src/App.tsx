e381799bf95a88c858b30a366e0e046da682a40e
JSH <qkdwn48@naver.com>
2022-05-07 23:58:03 +0900

    [타입] 기본 타입 학습 및 사용법
     - tsc 명령어
     프로젝트 명 node 경로

--- src/App.tsx
+++ src/App.tsx
@@ -1,42 +1,46 @@
 import { useState } from 'react'
-import logo from './logo.svg'
-import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  /**let count = 0; // 숫자
+  count += 1;
+  count = '갑자기 분위기 문자열'; // 이러면 에러가 납니다!
+
+  const message: string = 'hello world'; // 문자열
+
+  const done: boolean = true; // 불리언 값
+
+  const numbers: number[] = [1, 2, 3]; // 숫자 배열
+  const messages: string[] = ['hello', 'world']; // 문자열 배열
+
+  messages.push(1); // 숫자 넣으려고 하면.. 안된다!
+
+  let mightBeUndefined: string | undefined = undefined; // string 일수도 있고 undefined 일수도 있음
+  let nullableNumber: number | null = null; // number 일수도 있고 null 일수도 있음
+
+  let color: 'red' | 'orange' | 'yellow' = 'red'; // red, orange, yellow 중 하나임
+  color = 'yellow';
+  color = 'green'; // 에러 발생! */
+
+  function sum(x: number, y: number){
+    return x + y;
+  }
+
+  function sumArray(numbers: number[]): number {
+    returnNothing();
+    return numbers.reduce((acc, current) => acc + current, 0);
+  }
+
+  const total = sumArray([1, 2, 3, 4, 5]);
+
+  function returnNothing(): void {
+    console.log('I am just saying hello world');
+  }
 
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello Vite + React!</p>
-        <p>
-          <button type="button" onClick={() => setCount((count) => count + 1)}>
-            count is: {count}
-          </button>
-        </p>
-        <p>
-          Edit <code>App.tsx</code> and save to test HMR updates.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          {' | '}
-          <a
-            className="App-link"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
-        </p>
+        {sum(1,2)}
+        {total}
       </header>
     </div>
   )
