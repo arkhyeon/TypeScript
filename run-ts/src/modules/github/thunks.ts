534b03ccb53782dd184121bd1c4e6320062050be
JSH <qkdwn48@naver.com>
2022-05-21 23:54:24 +0900

    [타입] Thunk 리팩토링
    type Params = Parameters<F>; 는 함수의 파라미터들의 타입을 추론해줍니다. 이를 통하여 F 함수의 파라미터와 thunk 함수의 파라미터가 동일하게끔 설정

--- run-ts/src/modules/github/thunks.ts
+++ run-ts/src/modules/github/thunks.ts
@@ -1,20 +1,22 @@
-import { ThunkAction } from 'redux-thunk';
-import { RootState } from '../index';
 import { getUserProfile } from '../../api/github';
 import { getUserProfileAsync } from './actions';
-import { GithubAction } from './types';
+import createAsyncThunk from '../../lib/createAsyncThunk';
 
-export default function getUserProfileThunk(
-  username: string,
-): ThunkAction<void, RootState, null, GithubAction> {
-  return async (dispatch) => {
-    const { request, success, failure } = getUserProfileAsync;
-    dispatch(request());
-    try {
-      const userProfile = await getUserProfile(username);
-      dispatch(success(userProfile));
-    } catch (e: any) {
-      dispatch(failure(e));
-    }
-  };
-}
+const getUserProfileThunk = createAsyncThunk(getUserProfileAsync, getUserProfile);
+
+export default getUserProfileThunk;
+
+// export default function getUserProfileThunk(
+//   username: string,
+// ): ThunkAction<void, RootState, null, GithubAction> {
+//   return async (dispatch) => {
+//     const { request, success, failure } = getUserProfileAsync;
+//     dispatch(request());
+//     try {
+//       const userProfile = await getUserProfile(username);
+//       dispatch(success(userProfile));
+//     } catch (e: any) {
+//       dispatch(failure(e));
+//     }
+//   };
+// }
