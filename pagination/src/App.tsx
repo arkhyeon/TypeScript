bea7ca7d32336484795ede89bbcf6b93d50e38f2
JSH <qkdwn48@naver.com>
2022-06-04 23:59:11 +0900

    [init]test data

--- pagination/src/App.tsx
+++ pagination/src/App.tsx
@@ -3,18 +3,15 @@ import './App.css';
 
 function App() {
   // const content = 433;
-  const listNum = 10;
+  const listNum = [1,2,3,4,5,6,7,8,9,10];
   // const pageNum = 10;
 
-  const pagination = () => {
-    for (let i = 1; i < listNum; i++) {
-      return <div>{i}</div>;
-    }
-  };
   return (
     <div className="App">
       <header className="App-header">
-        <div>{() => pagination()}</div>
+        <div>{listNum.map((num) => {
+          return <div>{num}</div>;
+        })}</div>
       </header>
     </div>
   );
